2b8717fc088cdbd00d4898bf0da3e680537b364e
Thiago Costa <thiagohrcosta86@gmail.com>
2021-04-13 20:58:40 -0300

    Adding validations to Review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,6 @@
 class Review < ApplicationRecord
   validates :content, presence: true
-  validates
-
-
+  validates :rating, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
 
   belongs_to :restaurant
 end

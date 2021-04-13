87d5feaedfd57c97710193deb514cfc24ca4efdb
Thiago Costa <thiagohrcosta86@gmail.com>
2021-04-13 20:16:50 -0300

    Actions index, show and new created at controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,16 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaruant.new
+  end
+
+  def create
+  end
 end

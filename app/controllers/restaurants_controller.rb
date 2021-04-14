4780663ac9ad5e17135bf6a522593036564b8916
Thiago Costa <thiagohrcosta86@gmail.com>
2021-04-13 21:21:21 -0300

    adding faker

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -8,12 +8,35 @@ class RestaurantsController < ApplicationController
   end
 
   def new
-    @restaurant = Restaruant.new
+    @restaurant = Restaurant.new
   end
 
   def create
-    @restaurant = Restaruant.new()
+    @restaurant = Restaurant.new(restaurant_params)
     @restaurant.save
-    redirect_to restaruant_path(@restaurant)
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.new(params[:id])
+    @restaruant.update(restaurant_params)
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+
+    redirect_to restaurant_path
+  end
+
+  private
+
+  def restaruant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
